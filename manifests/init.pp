08b9464bd956b1868e5d92eb4db73267c6ce2c80
= <gorozco@gmail.com>
2014-01-05 15:20:12 -0600

    Remove default values for proxy

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class proxy ($http_proxy_host = "proxy-us.intel.com", $http_proxy_port = "911", $https_proxy_host = "proxy-us.intel.com", $https_proxy_port = "911", $ftp_proxy_host = "proxy-us.intel.com", $ftp_proxy_port = "911", $socks_proxy_host = "proxy-skype.intel.com", $socks_proxy_port = "1080", $no_proxy_domains = ".intel.com" ) {
+class proxy ($http_proxy_host = "", $http_proxy_port = "", $https_proxy_host = "", $https_proxy_port = "", $ftp_proxy_host = "", $ftp_proxy_port = "", $socks_proxy_host = "", $socks_proxy_port = "", $no_proxy_domains = "" ) {
   case $::operatingsystem {
     'centos', 'fedora', 'redhat', 'scientific': {
       $nc_package = "nc"

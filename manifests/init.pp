2552e9831c90569f8ae5d20c28bd7ae731e0974e
Geronimo Orozco <geronimo.orozco@intel.com>
2014-02-08 15:17:52 -0600

    Add corkscrew to use it to do ssh tunneling

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,22 @@
 class proxy ($http_proxy_host = "", $http_proxy_port = "", $https_proxy_host = "", $https_proxy_port = "", $ftp_proxy_host = "", $ftp_proxy_port = "", $socks_proxy_host = "", $socks_proxy_port = "", $no_proxy_domains = "" ) {
   case $::operatingsystem {
     'centos', 'fedora', 'redhat', 'scientific': {
-      $nc_package = "nc"
+      $packages  = [ "nc", "corkscrew" ]
       class { 'proxy::redhat': }
     }
     'debian', 'ubuntu': {
-      $nc_package = "netcat-openbsd"
+      $packages = [ "netcat-openbsd", "corkscrew" ]
       class { 'proxy::debian': }
     }
     'OpenSuSE', 'sles': {
-      $nc_package = "netcat-openbsd"
+      $packages = [ "netcat-openbsd", "corkscrew" ]
       class { 'proxy::suse': }
     }
     default: {
       fail("Module '${module_name}' is not currently supported by ${::operatingsystem}")
     }
   } 
-  package { $nc_package:
+  package { $packages:
    ensure => installed,
   }
 } 

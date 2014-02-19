87c16e4b59f7b5cf65053563bf2873c5f2c26c58
Geronimo Orozco <geronimo.orozco@intel.com>
2014-02-19 15:45:18 -0600

    Revert "Add corkscrew to use it to do ssh tunneling"
    This reverts commit 2552e9831c90569f8ae5d20c28bd7ae731e0974e.
    corkscrew is not proven to be the one and option that works on every place to replace socks_proxy

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,22 @@
 class proxy ($http_proxy_host = "", $http_proxy_port = "", $https_proxy_host = "", $https_proxy_port = "", $ftp_proxy_host = "", $ftp_proxy_port = "", $socks_proxy_host = "", $socks_proxy_port = "", $no_proxy_domains = "" ) {
   case $::operatingsystem {
     'centos', 'fedora', 'redhat', 'scientific': {
-      $packages  = [ "nc", "corkscrew" ]
+      $nc_package = "nc"
       class { 'proxy::redhat': }
     }
     'debian', 'ubuntu': {
-      $packages = [ "netcat-openbsd", "corkscrew" ]
+      $nc_package = "netcat-openbsd"
       class { 'proxy::debian': }
     }
     'OpenSuSE', 'sles': {
-      $packages = [ "netcat-openbsd", "corkscrew" ]
+      $nc_package = "netcat-openbsd"
       class { 'proxy::suse': }
     }
     default: {
       fail("Module '${module_name}' is not currently supported by ${::operatingsystem}")
     }
   } 
-  package { $packages:
+  package { $nc_package:
    ensure => installed,
   }
 } 
